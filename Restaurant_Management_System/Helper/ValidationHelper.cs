3089ca69cb3da9eb55ca1bff0828bc2ea127f486
madaljeh <ahmadmadaljeh@gmail.com>
2025-04-24 17:06:33 +0300

    Create ResetPassword API

---
+++ Restaurant_Management_System/Helper/ValidationHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace Restaurant_Management_System.Helpers.Validations
+{
+    public static class ValidationHelper
+    {
+        public static bool IsValidPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password) && password.Length >= 6)
+                throw new Exception("Password Is Required");
+            return true;
+        }
+        public static bool IsValidName(string name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name) || name.Length > 50)
+                throw new Exception("Name Is Required And Should not be more than 50 character");
+            foreach (char c in name)
+            {
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                    throw new Exception("Name Is Required To Contais of character and white spaces ");
+            }
+            return true;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                throw new Exception("Email Is  Required");
+
+            int atIndex = email.IndexOf('@');
+            int dotIndex = email.LastIndexOf('.');
+
+            if (atIndex < 1 || dotIndex < atIndex + 2 || dotIndex >= email.Length - 2)
+                throw new Exception("Email Is  Required");
+
+            string domain = email.Substring(atIndex + 1, dotIndex - atIndex - 1);
+            string extension = email.Substring(dotIndex + 1);
+
+            if (domain.Length < 2 || extension.Length < 2)
+                throw new Exception("Email Is  Required");
+
+            foreach (char c in email.Substring(0, atIndex))
+            {
+                if (!char.IsLetterOrDigit(c) && c != '.' && c != '_' && c != '%' && c != '+' && c != '-')
+                    throw new Exception("Email Is  Required");
+            }
+            return true ;
+        }
+    }
+}
