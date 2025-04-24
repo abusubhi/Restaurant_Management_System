642ac0c84c453f6003a82b4f787553c669e942ad
Eng Mohammed Aloshaibi <107276162+abusubhi@users.noreply.github.com>
2025-04-24 17:17:29 +0300

    Emplement SignUp API

--- Restaurant_Management_System/Controllers/AuthController.cs
+++ Restaurant_Management_System/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Restaurant_Management_System.DTOs.AuthDTO.Request;
+using System.Data;
 namespace Restaurant_Management_System.Controllers
 {
     [Route("api/[controller]")]
@@ -73,6 +74,48 @@ namespace Restaurant_Management_System.Controllers
                 return StatusCode(500,ex.Message);
             }
 
+        }
+
+
+        [HttpPost("MyCreateUser")]
+        public IActionResult CreateUser([FromBody] SignUpDTO user)
+        {
+            string connectionString = "Data Source=DESKTOP-8T8OGPF;Initial Catalog=RMS;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("CREATE_NEW_USER", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                        command.Parameters.AddWithValue("@LastName", user.LastName);
+                        command.Parameters.AddWithValue("@UserName", user.Username);
+                        command.Parameters.AddWithValue("@Password", user.Password);
+                        command.Parameters.AddWithValue("@UserRoleId", user.UserRoleId);
+                        command.Parameters.AddWithValue("@Email", user.Email);
+                        command.Parameters.AddWithValue("@PhoneNumber", user.PhoneNumber);
+                        command.Parameters.AddWithValue("@Image", user.Image ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@CreatedBy", user.CreatedBy);
+
+                        command.ExecuteNonQuery();
+
+                        return Ok(new { Message = "User created successfully" });
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(500, new { Message = "Database error", Error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "An error occurred", Error = ex.Message });
+            }
         }
     }
 }
