b1c545317287891c198063c58902f7f5fb170e8c
AbdullatifJamal <abdullatifsaleh2@gmail.com>
2025-04-24 17:45:06 +0300

    add login api

--- Restaurant_Management_System/Controllers/AuthController.cs
+++ Restaurant_Management_System/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Restaurant_Management_System.DTOs.AuthDTO.Request;
+using Restaurant_Management_System.DTOs.AuthDTO.Response;
 using System.Data;
 namespace Restaurant_Management_System.Controllers
 {
@@ -117,5 +118,50 @@ namespace Restaurant_Management_System.Controllers
                 return StatusCode(500, new { Message = "An error occurred", Error = ex.Message });
             }
         }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> SignIn(LoginDTO input)
+        {
+            var response = new LoginResponse();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(input.Email) || string.IsNullOrWhiteSpace(input.Password))
+                    throw new Exception("Email and Password are required");
+
+                string connectionString = "Data Source=DESKTOP-D89OGE0\\SQLEXPRESS;Initial Catalog=RMS-;Integrated Security=True;Trust Server Certificate=True";
+
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("User_Login", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Email", input.Email);
+                command.Parameters.AddWithValue("@Password", input.Password);
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                if (dataTable.Rows.Count == 0)
+                    throw new Exception("Invalid Email / Password");
+
+                if (dataTable.Rows.Count > 1)
+                    throw new Exception("Query Contains More Than One Element");
+
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    response.UserId = Convert.ToInt32(row["UserId"]);
+                    response.Username = row["Username"].ToString();
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An Error Was Occurred {ex.Message}");
+            }
+        }
+
     }
 }
---
+++ Restaurant_Management_System/DTOs/AuthDTO/Request/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant_Management_System.DTOs.AuthDTO.Request
+{
+    public class LoginDTO
+    {
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
